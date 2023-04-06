965e7f0f1ddb8b3a9be050589e787d2347dcc9b3
CarlSouza <85083641+CarlSouza@users.noreply.github.com>
2023-04-06 11:53:48 -0300

    Update printArray.cpp

--- revisão teste/printArray.cpp
+++ revisão teste/printArray.cpp
@@ -12,22 +12,34 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void printArray(int arr[], int size) {
-    cout << "{ ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i];
-        if (i < size - 1) {
-            cout << " , ";
+void printArray(int arr[],int N)
+{
+   cout<<" { ";
+   for (int i=0;i<=N ; i++ )
+   {
+       cout<<arr[i];
+        if (i<=N-1)
+        {
+          cout<<",";    
         }
-    }
-    cout << " }" << endl;
+   }
+   
+   cout<<" }";
 }
 
+
 int main() {
+    
+    int arr1[]={9,4,5,3,2,1};
+    int iN=sizeof(arr1) / sizeof(arr1[0]);//numero de elementos de um array ;
     int arr[] = {1, 1, 2, 3, 5, 8, 13};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     printArray(arr, size);
+    cout<<"\n ================="<<endl;
+    
+    printArray(arr1, iN);
 
     return 0;
 }
+
