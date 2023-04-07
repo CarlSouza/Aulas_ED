e4e16d0e9251bf57e6c13f98077e31b5aef32f56
CarlSouza <85083641+CarlSouza@users.noreply.github.com>
2023-04-07 11:32:57 -0300

    questao1_teste

---
+++ revisão teste/main (9).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string> // incluir este cabeçalho
+
+using namespace std;
+
+int main (){
+    
+  int iN;
+  cin>>iN;
+  //imprime o triangulo
+  cout<<"seu triângulo terá : "<<iN<<" linhas"<<endl;
+  
+  for (int i=0;i<=iN;i++)//colunas
+  {
+    for (int j=0;j<=i;j++)//linhas
+    {
+        cout<<"#" ;
+    } cout<<endl;
+    
+  }
+  // imprime o triangulo invertido:
+   for (int i=iN;i>=0;i--)//colunas
+  {
+    for (int j=i;j>=0;j--)//linhas
+    {
+        cout<<"#" ;
+    } cout<<endl;
+    
+  }
+  
+  return 0;
+}
