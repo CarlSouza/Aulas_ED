0057b2931ba32be89df36b176be26c2c4f8f9593
CarlSouza <85083641+CarlSouza@users.noreply.github.com>
2023-04-09 17:48:31 -0300

    Update main (4).cpp

--- revisão teste/main (4).cpp
+++ revisão teste/main (4).cpp
@@ -9,32 +9,32 @@ using std::endl;
 using std::string;
 
 
-// criar um função que determinar o vencedor
+// criar uma função que determinar o vencedor
 
-
+// imprime o tabuleiro:
 void print_game(char game[]) {
-    for (int i = 0; i < 9; i++) {
-        std::cout << game[i] << " ";
-        if ((i + 1) % 3 == 0) {
-            std::cout << std::endl;
+    for (int i = 0; i < 9; i++) {// tamanhdo do tabuleiro
+        cout << game[i] << " ";
+        if ((i + 1) % 3 == 0) {// quantidade de linhas
+           cout <<endl;
         }
     }
 }
-
+//confere o vencedor
 bool check_win(char game[], char symbol) {
-    // check rows
+    // por linhas
     for (int i = 0; i < 9; i += 3) {
         if (game[i] == symbol && game[i + 1] == symbol && game[i + 2] == symbol) {
             return true;
         }
     }
-    // check columns
+    // por colunas
     for (int i = 0; i < 3; i++) {
         if (game[i] == symbol && game[i + 3] == symbol && game[i + 6] == symbol) {
             return true;
         }
     }
-    // check diagonals
+    // por diagonais
     if (game[0] == symbol && game[4] == symbol && game[8] == symbol) {
         return true;
     }
@@ -43,19 +43,19 @@ bool check_win(char game[], char symbol) {
     }
     return false;
 }
-
+//move a jogada
 void make_move(char game[], int index, char symbol) {
     if (game[index] != ' ') {
-        std::cout << "Jogada inválida!" << std::endl;
+        cout << "Jogada inválida!" << endl;
         return;
     }
     game[index] = symbol;
     print_game(game);
     if (check_win(game, symbol)) {
-        std::cout << "Parabéns! O jogador '" << symbol << "' venceu!" << std::endl;
+        cout << "Parabéns! O jogador '" << symbol << "' venceu!" << endl;
     }
 }
-
+// teste da funcao
 int main() {
     char game[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
     print_game(game);
