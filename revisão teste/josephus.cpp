e4d6d90f9708269a73a3dc64f82bd8e4c289d3a0
CarlSouza <85083641+CarlSouza@users.noreply.github.com>
2023-04-10 13:53:29 -0300

    Update josephus.cpp

--- revisão teste/josephus.cpp
+++ revisão teste/josephus.cpp
@@ -1,23 +1,21 @@
-// tempo de jogo:
-
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 
-
-// josefo :usando recursão
-
 int josephus(int n, int k)//n-soldados ,k-esima morte
 {
-    if (n == 1)
+    if (n == 1){
         return 1;
-    else
+    }    
+    else{
         
         return (josephus(n - 1, k) + k - 1) % n + 1;
+        
+    }
 }
  
 // Dtestando a função
@@ -33,7 +31,5 @@ int main()
     cout << "O vencedor é  " << josephus(1234, 233)<<endl;
     return 0;
 }
- 
-// This code is contributed by shubhamsingh10
-   
+
 
